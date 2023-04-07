2c8ba066327da2dc5465e37252474558c70e530c
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-07 20:16:44 +0300

    Set up tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
+    colors: {
+      'orange': 'hsl(26, 100%, 55%)',
+      'pale-orange': 'hsl(25, 100%, 94%)',
+      'very-dark-blue': 'hsl(220, 13%, 13%)',
+      'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+      'grayish-blue': 'hsl(220, 14%, 75%)',
+      'light-grayish-blue': 'hsl(223, 64%, 98%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'black': 'hsl(0, 0%, 0%)',
+    },
+    fontFamily: {
+      'kumbh-sans': ['Kumbh Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
