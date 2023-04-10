4f7608fc8e11feec197c044857df38efbfc261f2
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-11 01:35:25 +0300

    Disable scroll when lightbox is open

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,6 +1,11 @@
 import ImageView from './image-view.js';
 
 export default function initalizeLightbox(mainImageView) {
+  const body = document.querySelector('body');
+  const lightbox = document.querySelector('#lightbox');
+  const openButton = document.querySelector('#open-lightbox-button');
+  const closeButton = lightbox.querySelector('#close-lightbox-button');
+
   const lightboxImageView = new ImageView(
     '#lightbox-image',
     '#lightbox-previous-button',
@@ -8,16 +13,14 @@ export default function initalizeLightbox(mainImageView) {
     'lightbox-image',
   );
 
-  const lightbox = document.querySelector('#lightbox');
-  const openButton = document.querySelector('#open-lightbox-button');
-  const closeButton = lightbox.querySelector('#close-lightbox-button');
-
   openButton.addEventListener('click', () => {
     lightbox.style.removeProperty('display');
+    body.style.setProperty('overflow', 'hidden');
     lightboxImageView.setCurrentImageIndex(mainImageView.currentImageIndex);
   });
 
   closeButton.addEventListener('click', () => {
     lightbox.style.setProperty('display', 'none');
+    body.style.removeProperty('overflow');
   });
 }
