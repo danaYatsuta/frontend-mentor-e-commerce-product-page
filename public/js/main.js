6e9ec501085f8461e34e17717035220db1755745
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-10 22:53:07 +0300

    Basic lightbox functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
 import initializeMobileMenu from './mobile-menu.js';
+import initalizeLightbox from './lightbox.js';
 
 initializeMobileMenu('#mobile-menu', '#open-menu-button', '#close-menu-button');
+initalizeLightbox('#lightbox', '#product-image', '#close-lightbox-button');
