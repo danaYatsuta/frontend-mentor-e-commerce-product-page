bc07792761622cdb47b04200c54d6d5a2e4002cd
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-11 01:14:34 +0300

    JS refactor

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,64 +1,23 @@
-import images from './images.js';
+import ImageView from './image-view.js';
 
-// TODO inital current image dependant on what image was selected
-// before opening lightbox
-
-let currentImage = 0;
-
-function incrementCurrentImage() {
-  currentImage = currentImage + 1 === images.length ? 0 : currentImage + 1;
-}
-
-function decrementCurrentImage() {
-  currentImage = currentImage - 1 === -1 ? images.length - 1 : currentImage - 1;
-}
-
-export default function initalizeLightbox(
-  lightboxSelector,
-  openButtonSelector,
-  closeButtonSelector,
-  lightboxImageSelector,
-  previousButtonSelector,
-  nextButtonSelector,
-) {
-  const lightbox = document.querySelector(lightboxSelector);
-  const openButton = document.querySelector(openButtonSelector);
-  const closeButton = lightbox.querySelector(closeButtonSelector);
-
-  const lightboxImage = lightbox.querySelector(lightboxImageSelector);
-  const previousButton = lightbox.querySelector(previousButtonSelector);
-  const nextButton = lightbox.querySelector(nextButtonSelector);
-
-  const radioButtons = lightbox.querySelectorAll(
-    'input[name="lightbox-image"]',
+export default function initalizeLightbox(mainImageView) {
+  const lightboxImageView = new ImageView(
+    '#lightbox-image',
+    '#lightbox-previous-button',
+    '#lightbox-next-button',
+    'lightbox-image',
   );
 
-  radioButtons.forEach((radioButton) => {
-    radioButton.addEventListener('click', () => {
-      if (radioButton.checked) {
-        currentImage = radioButton.value - 1;
-        lightboxImage.setAttribute('src', images[currentImage]);
-      }
-    });
-  });
+  const lightbox = document.querySelector('#lightbox');
+  const openButton = document.querySelector('#open-lightbox-button');
+  const closeButton = lightbox.querySelector('#close-lightbox-button');
 
   openButton.addEventListener('click', () => {
     lightbox.style.removeProperty('display');
+    lightboxImageView.setCurrentImageIndex(mainImageView.currentImageIndex);
   });
 
   closeButton.addEventListener('click', () => {
     lightbox.style.setProperty('display', 'none');
-  });
-
-  nextButton.addEventListener('click', () => {
-    incrementCurrentImage();
-    lightboxImage.setAttribute('src', images[currentImage]);
-    radioButtons[currentImage].checked = true;
-  });
-
-  previousButton.addEventListener('click', () => {
-    decrementCurrentImage();
-    lightboxImage.setAttribute('src', images[currentImage]);
-    radioButtons[currentImage].checked = true;
   });
 }
