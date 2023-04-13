d90367e4a99e17065cc2ba1ebf3c5049b75852bb
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-13 17:15:48 +0300

    Animate lightbox

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -15,12 +15,28 @@ export default function initalizeLightbox(mainImageView) {
 
   openButton.addEventListener('click', () => {
     lightbox.style.removeProperty('display');
+    setTimeout(() => {
+      lightbox.classList.add('opacity-100');
+      lightbox.classList.remove('opacity-0');
+    }, 0);
     body.style.setProperty('overflow', 'hidden');
+
     lightboxImageView.setCurrentImageIndex(mainImageView.currentImageIndex);
   });
 
   closeButton.addEventListener('click', () => {
-    lightbox.style.setProperty('display', 'none');
+    lightbox.classList.add('opacity-0');
+    lightbox.classList.remove('opacity-100');
     body.style.removeProperty('overflow');
+
+    if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+      lightbox.style.setProperty('display', 'none');
+    }
+  });
+
+  lightbox.addEventListener('transitionend', () => {
+    if (lightbox.classList.contains('opacity-0')) {
+      lightbox.style.setProperty('display', 'none');
+    }
   });
 }
