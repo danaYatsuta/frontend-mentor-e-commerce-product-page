c8fe1ec894852c5dc6e6bab037228b74e63419f6
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-13 15:23:19 +0300

    Animate mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,13 +4,32 @@ export default function initializeMobileMenu() {
   const openButton = document.querySelector('#open-mobile-menu-button');
   const closeButton = document.querySelector('#close-mobile-menu-button');
 
+  let menuOpen = false;
+
   openButton.addEventListener('click', () => {
     menu.style.removeProperty('display');
+    setTimeout(() => {
+      menu.classList.add('bg-opacity-75');
+      menu.classList.remove('bg-opacity-0');
+      menu.firstElementChild.classList.remove('-translate-x-full');
+    }, 0);
     body.style.setProperty('overflow', 'hidden');
+
+    menuOpen = true;
   });
 
   closeButton.addEventListener('click', () => {
-    menu.style.setProperty('display', 'none');
+    menu.classList.add('bg-opacity-0');
+    menu.classList.remove('bg-opacity-75');
+    menu.firstElementChild.classList.add('-translate-x-full');
     body.style.removeProperty('overflow');
+
+    menuOpen = false;
+  });
+
+  menu.addEventListener('transitionend', () => {
+    if (!menuOpen) {
+      menu.style.setProperty('display', 'none');
+    }
   });
 }
